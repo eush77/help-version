8f3cf764df46bff0315fe378447016fb4fecca62
Eugene Sharygin <eush77@gmail.com>
2015-05-23 14:39:35 +0300

    Init

---
+++ README.md
@@ -0,0 +1,22 @@
+[![npm](https://nodei.co/npm/help-version.png)](https://nodei.co/npm/help-version/)
+
+# help-version
+
+[![Build Status][travis-badge]][travis] [![Dependency Status][david-badge]][david]
+
+Easily handle --help and --version arguments in your CLI application.
+
+[travis]: https://travis-ci.org/eush77/help-version
+[travis-badge]: https://travis-ci.org/eush77/help-version.svg
+[david]: https://david-dm.org/eush77/help-version
+[david-badge]: https://david-dm.org/eush77/help-version.png
+
+## Install
+
+```
+npm install help-version
+```
+
+## License
+
+MIT
---
+++ test/test.js
@@ -0,0 +1,53 @@
+'use strict';
+
+var test = require('tape'),
+    concat = require('concat-stream');
+
+var spawn = require('child_process').spawn,
+    path = require('path');
+
+
+var getOutput = function (/* [argv] */ cb) {
+  var argv = [].slice.call(arguments);
+  cb = argv.pop();
+
+  var app = spawn('node', [path.join(__dirname, 'app/cli.js')].concat(argv),
+                  { stdio: ['ignore', 'pipe', 'inherit'] });
+  app.on('exit', function (code, signal) {
+    code |= !!signal;
+    if (code) {
+      process.exit(code);
+    }
+  });
+
+  app.stdout.pipe(concat({ encoding: 'string' }, cb));
+};
+
+
+test('normal output', function (t) {
+  t.plan(2);
+
+  getOutput(function (output) {
+    t.equal(output, 'Main output.\n');
+  });
+
+  getOutput('--prefix', 'app', '--adj', 'Some other', function (output) {
+    t.equal(output, '[app] Some other output.\n');
+  });
+});
+
+
+test('--help', function (t) {
+  getOutput('--help', function (output) {
+    t.equal(output, 'Usage information.\n');
+    t.end();
+  });
+});
+
+
+test('--version', function (t) {
+  getOutput('--version', function (output) {
+    t.equal(output, 'v10.130.209\n');
+    t.end();
+  });
+});
